b978b065aa53a5f5269a2f40e553c532dafe46ec
pt807 <pt807@naver.com>
2023-01-19 10:33:55 +0900

    user #3

--- mysite02/src/main/java/com/douzone/mysite/dao/UserDao.java
+++ mysite02/src/main/java/com/douzone/mysite/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.douzone.mysite.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.douzone.mysite.vo.UserVo;
@@ -43,6 +44,54 @@ public class UserDao {
 		}
 	}
 
+	public UserVo findbyEmailAndPassword(UserVo vo) {
+		UserVo result = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = "select * from user where email = ? and password = password(?)";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getEmail());
+			pstmt.setString(2, vo.getPassword());
+
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = new UserVo();
+				
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				
+				result.setNo(no);
+				result.setName(name);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
+
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 
@@ -56,4 +105,5 @@ public class UserDao {
 
 		return conn;
 	}
+
 }
