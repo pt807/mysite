ff3d2cf348c1c06b4e5736ed6017991b172bfb62
pt807 <pt807@naver.com>
2023-01-31 16:53:33 +0900

    guestbook #finish

--- mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
@@ -2,22 +2,27 @@ package com.douzone.mysite.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.douzone.mysite.repository.GuestbookRepository;
 import com.douzone.mysite.vo.GuestbookVo;
 
 @Service
 public class GuestbookService {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
 
 	public List<GuestbookVo> getMessageList() {
-		return null;
-	}
-
-	public void deleteMessage(Long no, String password) {
-
+		return guestbookRepository.findAll();
 	}
 
 	public void addMessage(GuestbookVo Vo) {
-
+		guestbookRepository.insert(Vo);
 	}
+
+	public void deleteMessage(Long no, String password) {
+		guestbookRepository.deleteByPassword(password, no);
+	}
+
 }
