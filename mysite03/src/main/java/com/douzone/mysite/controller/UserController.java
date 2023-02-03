1bd5c87c36c0925b444977ead83694c6d0c65dbe
pt807 <pt807@naver.com>
2023-02-03 12:49:08 +0900

    Bean Validation

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.douzone.mysite.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,8 +25,18 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String join(UserVo vo) {
-		userService.join(vo);
+	public String join(@Valid UserVo vo, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError err : list) {
+//				System.out.println(err);
+//			}
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
+		
+		// userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 
