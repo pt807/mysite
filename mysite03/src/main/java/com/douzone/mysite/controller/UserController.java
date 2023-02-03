47b20e5dba437d26e34865a9bf801a7075ad2f72
pt807 <pt807@naver.com>
2023-02-03 13:52:55 +0900

    validation #final

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,23 +21,24 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
-	public String join() {
+	public String join(UserVo vo) {
 		return "user/join";
 	}
 
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String join(@Valid UserVo vo, BindingResult result, Model model) {
-		
-		if(result.hasErrors()) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
 //			List<ObjectError> list = result.getAllErrors();
 //			for(ObjectError err : list) {
 //				System.out.println(err);
 //			}
+			// model.addAttribute("userVo", vo);
 			model.addAllAttributes(result.getModel());
 			return "user/join";
 		}
-		
-		// userService.join(vo);
+
+		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 
@@ -92,10 +94,10 @@ public class UserController {
 			return "redirect:/";
 		}
 		////////////////////////////////////////////////////
-		
+
 		vo.setNo(authUser.getNo());
 		userService.updateUser(vo);
-		
+
 		authUser.setName(vo.getName());
 		return "redirect:/";
 	}
