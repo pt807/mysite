114232bed54c4117d44e45b77ce99a5cfa2387be
pt807 <pt807@naver.com>
2023-02-09 09:57:58 +0900

    site - Spring bean #1

---
+++ mysite03/src/main/java/com/douzone/mysite/event/ApplicationContextEventListener.java
@@ -0,0 +1,25 @@
+package com.douzone.mysite.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+
+import com.douzone.mysite.service.SiteService;
+import com.douzone.mysite.vo.SiteVo;
+
+public class ApplicationContextEventListener {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@EventListener({ ContextRefreshedEvent.class })
+	public void handleApplicationContextRefreshEvent() {
+		System.out.println(((Object)applicationContext.getClass()));
+		System.out.println(System.identityHashCode(applicationContext));
+		System.out.println("--- Context Refresh Event Received ---" + applicationContext);
+		
+		SiteService service = applicationContext.getBean(SiteService.class);
+		SiteVo site = service.getSite();
+	}
+}
